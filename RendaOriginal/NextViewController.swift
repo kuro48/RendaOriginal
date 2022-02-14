ada7fd9e045c2595a9b23733c478eb2be1796e0d
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2022-02-14 17:03:39 +0900

    RendaOrIginal!!

---
+++ RendaOriginal/NextViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NextViewController.swift
+//  RendaOriginal
+//
+//  Created by 黒川龍之介 on 2022/02/14.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    @IBOutlet var resultLabel : UILabel!
+    @IBOutlet var resultTimer: UILabel!
+    
+    var result:Int = 0
+    var resultText : String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultLabel.text = String(result)
+        hantei()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func back(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func hantei(){
+        if result >= 140{
+            resultTimer.text = "WONDERFULL‼︎"
+        }else if result >= 100{
+            resultTimer.text = "PERFECT‼︎"
+        }else if result >= 80{
+            resultTimer.text = "GREAT‼︎"
+        }else{
+            resultTimer.text = "GOOD‼︎"
+        }
+    }
+}
--- RendaOriginal/ViewController.swift
+++ RendaOriginal/ViewController.swift
@@ -4,16 +4,59 @@
 //
 //  Created by 黒川龍之介 on 2022/02/14.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+
+    @IBOutlet var countNumber: UILabel!
+    @IBOutlet var countBtn: UIButton!
+    @IBOutlet var timerNumber: UILabel!
+    
+    var number = 0
+    var timerCount = 30.00
+    var timer: Timer = Timer()
+    var result = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func count() {
+        number = number + 1
+        countNumber.text = String(number)
+    }
+    
+    @objc func down() {
+        if timerCount < 0.01 {
+            result = number
+            self.performSegue(withIdentifier: "toNext", sender: result)
+            timer.invalidate()
+        }
+        
+        timerCount = timerCount - 0.01
+        timerNumber.text = String(format: "%.2f",timerCount)
 
+    }
+    
+    @IBAction func start(){
+        timerCount = 30.00
+        if !timer.isValid{
+                    timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                                 target: self,
+                                                 selector: #selector(self.down),
+                                                 userInfo: nil,
+                                                 repeats: true)
+                }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue,sender: Any?){
+        if segue.identifier == "toNext" {
+                let toNext = segue.destination as! NextViewController
+                toNext.result = self.result
+        }
+    }
 }
+
 
